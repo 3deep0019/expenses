758b6801f8406ae24830fda6bf60ae531cf117f3
TRIDEEP GHOSH <41672219+3deep0019@users.noreply.github.com>
2022-04-21 13:30:26 +0530

    jwt token test

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const connectDB = require('../db/connection')
+const jwt = require('jsonwebtoken');
 
 connectDB()
 const app = express()
 const port = process.env.PORT || 2000
 
 app.get('/', (req, res) => res.send('Hello World'))
+app.get('/tokenString',(req,res)=>{
+    res.send(_tokenGenerator())
+})
+const _tokenGenerator = () => {
+    const accessToken = {
+        ExpiresAt: Date.now() + 15 * 24 * 60 * 60 * 1000,
+        Type: 'Application',
+    };
+    accessToken.TokenString = jwt.sign(accessToken, 'MDA3IDAwNyAwMDcgMDA3', {
+        algorithm: 'HS256',
+    });
+    return accessToken;
+};
+
 
 app.listen(port)
